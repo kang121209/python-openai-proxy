d9b053211c4f17e40eab844d5bff2430e35dfff0
kang121209 <127708228+kang121209@users.noreply.github.com>
2023-03-19 16:30:44 +0800

    Create proxy.py

---
+++ proxy.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_proxy import Proxy
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    return Proxy('https://api.openai.com').forward()
+
+if __name__ == '__main__':
+    app.run(port=9000)
