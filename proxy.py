471473e6b4f55b6454e81175cac49c37f2795546
kang121209 <127708228+kang121209@users.noreply.github.com>
2023-03-19 18:37:10 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 API_HOST='https://api.openai.com/'
+OPENAI_API_KEY='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'
 
 @app.route('/', defaults={'path': ''})  
 @app.route('/path:path')
