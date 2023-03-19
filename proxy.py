f464478a5ed05c050077d402dc5922973152890b
kang121209 <127708228+kang121209@users.noreply.github.com>
2023-03-19 20:01:04 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,18 +1,38 @@
 #-*-coding:utf-8-*-
-from flask import Flask
-from flask_http_proxy_middleware import HTTPProxyMiddleware
+from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
-app.config["PROXY_MIDDLEWARE_URL"] = "https://api.openai.com"
-app.config["PROXY_MIDDLEWARE_HEADERS"] = {"Access-Control-Allow-Origin": "*"}
-app.config["PROXY_MIDDLEWARE_HOSTS"] = ("*",)
 
-http_proxy_middleware = HTTPProxyMiddleware(app)
+TARGET_HOST='https://api.openai.com'
 
-@app.route("/")
-def proxy():
-    return http_proxy_middleware.handle_request()
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
+def proxy(path):
+    target_url = f'{TARGET_HOST}/{path}'
+    headers = {key: value for (key, value) in request.headers if key != 'Host'}
+    method=request.method
+    url=target_url
+    data=request.get_data()
+    params=request.args
+    cookies=request.cookies
+    print(target_url,headers,method,url,data,params,cookies)
+    response = requests.request(method=request.method,
+        url=target_url,
+        data=request.get_data(),
+        params=request.args,
+        headers=headers,
+        cookies=request.cookies,
+        allow_redirects=False)
 
-if __name__ == "__main__":
-    app.run(port=9000)
+    response_headers = [(name, value) for (name, value) in response.raw.headers.items()]
+    response_headers.append(('Access-Control-Allow-Origin', '*'))
+
+    response = Response(response.content, response.status_code, response_headers)
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
